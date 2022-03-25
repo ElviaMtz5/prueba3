a82ffcaf961a0a168689762cb525c9e274bb6832
ElviaMtz5 <anaelvia.martinez.g@gmail.com>
2022-03-24 20:10:13 -0600

    agregando el formato correcto al código

--- scr/calc.py
+++ scr/calc.py
@@ -1,18 +1,27 @@
-<<<<<<< HEAD
-def get_fractions(valor):
-  if("/" in valor):
-    decimal = int (valor[0:1]) / int (valor[2:3])
-    return decimal
-  else:
-    return float(valor)
+'#<<<<<<< HEAD'
 
-def suma(a,b):
-  sumandoa = get_fractions(a)
-  sumandob = get_fractions(b)
-  return sumandoa + sumandob
-=======
-def multiplica(a,b):
-  multiplicando = get_fractions(a)
-  multiplicador = get_fractions(b)
-  return multiplicando * multiplicador
->>>>>>> mult
+
+def get_fractions(valor):
+    if("/" in valor):
+        decimal = int(valor[0:1]) / int(valor[2:3])
+        return decimal
+    else:
+        return float(valor)
+
+
+def suma(a, b):
+    sumandoa = get_fractions(a)
+    sumandob = get_fractions(b)
+    return sumandoa + sumandob
+
+
+'#======='
+
+
+def multiplica(a, b):
+    multiplicando = get_fractions(a)
+    multiplicador = get_fractions(b)
+    return multiplicando * multiplicador
+
+
+'#>>>>>>> mult'
