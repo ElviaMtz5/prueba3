57061ac39e79b9904c01c312e1909355d04a1180
ElviaMtz5 <anaelvia.martinez.g@gmail.com>
2022-03-24 20:47:45 -0600

    agrefando el formato correcto al código en términos del PEP8 y añadiendo docstrings

--- scr/calc.py
+++ scr/calc.py
@@ -2,6 +2,10 @@
 
 
 def get_fractions(valor):
+    """
+    Descripción: Esta fucnión recibe un "valor" en forma de 
+    fracción el cual convierte a decimal 
+    """
     if("/" in valor):
         decimal = int(valor[0:1]) / int(valor[2:3])
         return decimal
@@ -10,6 +14,19 @@ def get_fractions(valor):
 
 
 def suma(a, b):
+    """
+    Descripción: Esta fucnión recibe dos valores los cuales pueden ser fracciones,
+    dichos valores serán sumados 
+
+    Parametrers:
+        a - es el primer sumando
+        b - es el segundo sumando 
+
+    Returns
+        Regresa la suma del primer sumando más el segundo sumando
+    """
+
+
     sumandoa = get_fractions(a)
     sumandob = get_fractions(b)
     return sumandoa + sumandob
@@ -19,9 +36,24 @@ def suma(a, b):
 
 
 def multiplica(a, b):
+    """
+    Descripción: Esta fucnión recibe dos valores los cuales pueden ser valores en fracciones,
+    dichos valores serán multiplicados
+
+    Parametrers:
+        a - es el multiplicando
+        b - es el multiplicador 
+
+    Returns
+        Regresa la multiplicación del multiplicando y el multiplicador
+    """
+
+
     multiplicando = get_fractions(a)
     multiplicador = get_fractions(b)
     return multiplicando * multiplicador
 
 
-'#>>>>>>> mult'
+'#>>>>>>> mult
+
+pip install pyment
